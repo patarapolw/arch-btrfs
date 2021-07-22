208aad3a0e5deb5f10f31b276852e1f28dd0a509
Pacharapol Withayasakpunt <polv@polv.cc>
2021-07-22 21:00:20 +0700

    add initial instruction

---
+++ scripts/00-esp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+fdisk -l
+
+read -r -p "Please choose the partition name: " PART
+parted $PART -- mklabel gpt
+
+parted $PART -- mkpart ESP fat32 1MiB 512MiB
+parted $PART -- mkpart primary 512MiB 100%
+
+lsblk
+
+read -r -p "Please choose the partition to format to EFI partition: " EFI
+mkfs.fat -L EFI $EFI
