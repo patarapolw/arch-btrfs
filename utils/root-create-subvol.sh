acb6ffd5a7f629560d21edd03501e9640e65eaa0
Pacharapol Withayasakpunt <polv@polv.cc>
2021-07-31 12:00:52 +0700

    rename folders

--- utils/root-create-subvol.sh
+++ utils/root-create-subvol.sh
@@ -6,8 +6,6 @@
 USER=polv
 BTRFS=/dev/sda5
 FOLDER="/home/polv/.local/share/Trash"
-N_FIRST=5
-N_CURRENT=$(btrfs sub get-default / | grep -oP '(?<=ID )[0-9]+')
 
 MNT="${FOLDER:1}"
 MNT="${MNT//\//_}"
@@ -15,18 +13,12 @@ MNT="${MNT//\//_}"
 shopt -s dotglob
 
 mkdir -p $FOLDER
-btrfs sub set-default $N_FIRST /
-mount $BTRFS /mnt
+mount $BTRFS -o subvolid=5 /mnt
 btrfs sub create /mnt/@/$MNT
 chattr +C /mnt/@/$MNT
 chown -R $USER /mnt/@/$MNT
 mv $FOLDER/* /mnt/@/$MNT
-btrfs sub set-default $N_CURRENT /
 umount /mnt
-
-# btrfs sub list /
-# echo 'get the id of that newly created subvol and add the thing to /etc/fstab'
-# echo 'when ready, type mount -a'
 
 echo "$BTRFS $FOLDER   btrfs   rw,noatime,compress=zstd:15,ssd,space_cache,subvolid=$(btrfs sub list / | grep "@/$MNT" | grep -oP '(?<=ID )[0-9]+'),subvol=/@/$MNT,discard=async,nodatacow 0 0" >> /etc/fstab
 mount -a
