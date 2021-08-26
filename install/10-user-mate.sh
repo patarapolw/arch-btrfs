22cc72d00e0da88453f272191241c786caa62bc8
Pacharapol Withayasakpunt <polv@polv.cc>
2021-08-26 18:04:54 +0700

    update

--- install/10-user-mate.sh
+++ install/10-user-mate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo pacman -S mate mate-extra mate-media blueman xorg network-manager-applet lightdm
-paru -S lightdm-slick-greeter lightdm-settings numlockx
+sudo pacman -S mate mate-extra mate-media blueman xorg network-manager-applet lightdm numlockx
+paru -S lightdm-slick-greeter lightdm-settings mate-menu mate-tweak
 
 sudo systemctl enable lightdm
 sudo systemctl enable bluetooth
