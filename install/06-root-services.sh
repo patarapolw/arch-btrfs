68e96ca996b7aa18082a6d194c1d14b53bccfd04
Pacharapol Withayasakpunt <polv@polv.cc>
2021-10-06 13:51:03 +0700

    fix errors in refind branch

--- install/06-root-services.sh
+++ install/06-root-services.sh
@@ -27,6 +27,7 @@ systemctl enable systemd-oomd
 echo "Enabling Snapper and automatic snapshots entries."
 systemctl enable snapper-timeline.timer
 systemctl enable snapper-cleanup.timer
+systemctl enable grub-btrfs.path
 
 # Setting umask to 077.
 sed -i 's/022/077/g' /etc/profile
