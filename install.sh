cb51689a53a9f0c156e63744d650dd3975efc4a9
Pacharapol Withayasakpunt <polv@polv.cc>
2021-10-12 21:56:01 +0700

    rename folders

--- install.sh
+++ install.sh
@@ -1,18 +1,29 @@
 #!/bin/bash -e
 
-STEPS_ROOT=two-drives
+ROOT=$(git rev-parse --show-toplevel)
+SQUASHFS=/tmp/arch-btrfs.squashfs
 MOUNT_POINT=/tmp/mnt
+CFG=config.yaml
 
 mkdir -p $MOUNT_POINT
-
 STATE_FILE=$MOUNT_POINT/.state
 
 if [[ ! -f $STATE_FILE ]]; then
-    mksquashfs $(git rev-parse --show-toplevel) /tmp/arch-btrfs.squashfs
-    mount /tmp/arch-btrfs.squashfs $MOUNT_POINT -t squashfs -o loop
+    mksquashfs $ROOT $SQUASHFS
+    mount $SQUASHFS $MOUNT_POINT -t squashfs -o loop
 
     echo 'init' > $STATE_FILE
 fi
 
-if [[ $(cat $STATE_FILE) == 'init' ]]; then
+cd $MOUNT_POINT
+
+BTRFS=$(cat config.yaml | yq '.mount."/".device')
+ESP=$(cat config.yaml | yq '.mount."/boot/efi".device')
+
+if [[ $(tail -n1 $STATE_FILE) == 'init' ]]; then
+    if $(cat config.yaml | yq '.mount."/boot/efi".format'); then
+        mkfs.vfat $ESP
+    fi
+
+    LABEL=$(cat config.yaml | yq '.mount."/".label') ./lib/01-format/01-format-arch.sh
 fi
